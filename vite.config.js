106f38fc2c1a7643ca8a4a127394fcc6d43b72e2
wreckloud <1677820334.com>
2025-06-07 17:15:35 +0800

    导入了elementplus

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// 引入自动导入ElementPlus依赖
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    // 自动导入ElementPlus组件
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   resolve: {
+    // 真正的修改路径
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
