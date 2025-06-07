cbe255252f35cfe0a344f32e6ac6d923c51f7c57
wreckloud <1677820334.com>
2025-06-07 15:37:53 +0800

    修复了修复图片路径导致的部署失败的问题

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+
+// 引入ElementPlus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+        AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
     vue(),
     vueDevTools(),
   ],
