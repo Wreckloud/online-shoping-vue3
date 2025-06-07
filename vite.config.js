ee504b5dfdd7e1a3bac45a885a0e733c9b89e46f
wreckloud <1677820334.com>
2025-06-07 17:50:33 +0800

    定制了ElementPlus主题色

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+// 自定义ElementPlus
+import ElementPlus from 'unplugin-element-plus/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -19,13 +22,29 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass' // 这里要改成sass
+        })
+      ]
+    }),
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true
     })
   ],
   resolve: {
     // 真正的修改路径
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
     }
   }
 })
