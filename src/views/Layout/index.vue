91c2edb271727785c8d037e38bd2cb276b323c4f
wreckloud <1677820334.com>
2025-06-08 18:20:48 +0800

    完成吸附式导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,13 +2,20 @@
 import AppFooter from './components/AppFooter.vue'
 import AppHeader from './components/AppHeader.vue'
 import AppNav from './components/AppNav.vue'
+import HeaderFixed from './components/HeaderFixed.vue'
 </script>
 
 <template>
   <AppNav />
+  <HeaderFixed />
   <AppHeader />
   <router-view />
+  <div class="test"></div>
   <AppFooter />
 </template>
 
-<style scoped></style>
+<style scoped>
+.test {
+  height: 1200px;
+}
+</style>
